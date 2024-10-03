069833fc78ee042f8025551de9013e9c6f3e2ea5
RyanS <ryanweeney157@gmail.com>
2024-10-03 10:04:59 -0400

    made calculateREsult function

--- calc.js
+++ calc.js
@@ -4,12 +4,25 @@ function clearDisplay() {
     display.value = '';
 }
 
+//gets rid of part of string technically
 function deleteNum() {
     var currentValue = display.value;
     display.value = currentValue.substring(0, currentValue.length -1);
 
 }
 
+//gets value of element and adds it to the calculator
 function addToCalc(value) {
     display.value += value;
+}
+
+
+//getscurent expression, validates it, and evaluates
+function calculateResult() {
+    var expression = display.value;
+    if (validExpression(expression)) {
+        display.value = eval(expression);
+    } else {
+        display.value = 'error'
+    }
 }
